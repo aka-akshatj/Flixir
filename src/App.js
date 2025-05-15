eb6a6759c783a129acbd8a05d4bc7b25a63647f2
Akshat Jain <akshat1910j@gmail.com>
2025-05-15 11:52:22 +0530

    account and saved

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ import {
 import Navbar from "./containers/Navbar/Navbar";
 // import UserPanel from "./containers/UserPanel/UserPanel";
 import HomePage from "./Pages/HomePage";
+// import AddPost from "./Pages/AddPost";
+import Saved from "./Pages/Saved";
+import Account from "./Pages/Account";
 import MobileMenu from "./containers/MobileMenu/MobileMenu";
 import Auth from "./containers/Auth/Auth";
-
+import PostDetails from "./components/PostDetails/PostDetails";
 
 function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
@@ -44,9 +47,14 @@ function App() {
                   )
                 }
               ></Route>
-
               <Route path="/posts" element={<HomePage></HomePage>}></Route>
               <Route path="/auth" element={<Auth />}></Route>
+              <Route
+                path="/posts/:id"
+                element={<PostDetails></PostDetails>}
+              ></Route>
+              <Route path="/saved" element={<Saved></Saved>}></Route>
+              <Route path="/account" element={<Account></Account>}></Route>
             </Routes>
           </div>
         </div>
