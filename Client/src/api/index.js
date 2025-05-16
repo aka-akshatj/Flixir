5aa79b806633d424182824067410241ff1a7b020
Akshat Jain <akshat1910j@gmail.com>
2025-05-17 03:28:02 +0530

    backend issues solved

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: "https://polaroid-webapp.herokuapp.com/" });
-const API = axios.create({ baseURL: "http://localhost:3000/" });
+const API = axios.create({ baseURL: "http://localhost:5000/" });
 
 //send token to backend
 API.interceptors.request.use((req) => {
